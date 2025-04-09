c492bad95f1c02b393450eaacf98f3df33faef81
ramen <48982211+KikuchiTomo@users.noreply.github.com>
2025-04-09 14:27:13 +0900

    Update MessageListView.swift

--- BariPort/Features/Message/Screens/MessageList/MessageListView.swift
+++ BariPort/Features/Message/Screens/MessageList/MessageListView.swift
@@ -23,19 +23,16 @@ class MessageListViewController: UIViewController, UITableViewDelegate, UITableV
     override func viewDidLoad() {
         super.viewDidLoad()    
         self.title = LocalizationString.directMessageTabBarItemLabel.localized
-        // TODO: 画像にしたほうがいいかも
+       
         let serviceNameLabel = UILabel()
         serviceNameLabel.text = "BariPort"
         serviceNameLabel.font = UIFont.boldSystemFont(ofSize: 20)
         serviceNameLabel.textColor = .systemCyan
-        // コンテナビューを作成し、左側に余白を追加します。
+        // コンテナビューを作成し、左側に余白を追加
         let containerView = UIView()
-        // ここでのX座標を調整することで、余白のサイズを変更できます。
-        // 例えば、X座標を10に設定することで、左側に10ポイントのスペースが追加されます。
+       
         serviceNameLabel.frame = CGRect(x: 20, y: 0, width: serviceNameLabel.intrinsicContentSize.width, height: serviceNameLabel.intrinsicContentSize.height)
         
-        // コンテナビューのサイズを、内包するラベルのサイズに基づいて設定します。
-        // ここでは、ラベルの幅に余白分を追加しています。
         containerView.frame = CGRect(x: 0, y: 0, width: serviceNameLabel.frame.width + 10, height: serviceNameLabel.frame.height)
         containerView.addSubview(serviceNameLabel)
         
@@ -55,7 +52,7 @@ class MessageListViewController: UIViewController, UITableViewDelegate, UITableV
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+
         self.presenter.viewDidAppear()
     }
         
